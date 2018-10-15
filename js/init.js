ddf14bc3613fc106079286ff45b6603cfb5c4183
Ritom <ritomsonowal99@gmail.com>
2018-10-15 18:23:54 +0530

    Initial Commit

---
+++ js/init.js
@@ -0,0 +1,30 @@
+// Navbar
+
+$(document).ready(function() {
+  $(window).on("scroll", function() {
+    if ($(window).scrollTop() >= 20) {
+      $(".navbar").addClass("compressed");
+    } else {
+      $(".navbar").removeClass("compressed");
+    }
+  });
+});
+
+// SmoothScroll
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
